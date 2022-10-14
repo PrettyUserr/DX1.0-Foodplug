9a5023e1a75599ccaab012bcb0c53228ae58dd1e
PrettyUserr <techbabyy@gmail.com>
2022-10-14 12:44:45 +0100

    completed header section for both large ns dmall screens

---
+++ starter/main.js
@@ -0,0 +1,19 @@
+
+// ES5
+
+// console.log("send me your account number")
+// alert(writeup)
+// var writeup = "i am soo tired"
+
+var hamburgerdiv = document.querySelector(".hamburger");
+var mobilelink = document.querySelector(".mobile-link-holder");
+var mybackdrop = document.querySelector(".backdrop")
+// console.log(hamburgerdiv)
+
+
+function duhhiknow() {
+    hamburgerdiv.classList.toggle("showburger")
+    mobilelink.classList.toggle("show-mobile-link-holder")
+    mybackdrop.classList.toggle("show-backdrop")
+    
+}
